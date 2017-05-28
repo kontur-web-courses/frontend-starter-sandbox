deb886508e6189872a4742f27dc8ee78fdc54da3
aminopyrodin <aminopyrodin@gmail.com>
2017-05-28 15:18:33 +0500

    replace loader to use

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   devtool: "source-map",
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+      { test: /\.js$/, exclude: /node_modules/, use: ["babel-loader"] }
     ]
   }
 };
